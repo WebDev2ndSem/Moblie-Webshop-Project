605c04d85215307d8e49c82eacee2fd377934bc9
Oskar Murand <oskar.murand@gmail.com>
2015-05-03 19:02:03 +0200

    .

---
+++ Resources/android/alloy/styles/deviceMenu.js
@@ -0,0 +1,1 @@
+module.exports = [{"isClass":true,"priority":10000.0002,"key":"container","style":{}},{"isClass":true,"priority":10000.0003,"key":"menuBtn","style":{width:"60%",height:"50dp",top:"10dp",borderRadius:"3dp",backgroundColor:"#00aeef",bordercolor:"#bbbbbb",}}];
